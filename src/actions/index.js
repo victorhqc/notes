b3ff2d7971412d8cca4bafef619ff74d2116ec59
victorhqc <victorhqc@gmail.com>
2016-05-08 18:44:20 -0500

    Simplified reducers and actions Although some of the legacy code is still there, I'll not use it for now.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,2 @@
 export * from 'filters';
 export * from 'notes';
-export * from 'note';
-export * from 'textNote';
--- src/reducers/notes.js
+++ src/reducers/notes.js
@@ -1,40 +1,8 @@
 import {
     REQUEST_NOTES,
     RECEIVE_NOTES,
-    FAIL_RECEIVE_NOTES,
-    SET_NOTE_COLOR,
-    ADD_TEXT_NOTE,
-    EDIT_NOTE,
-    DELETE_NOTE,
-    REQUEST_SAVE_NOTE,
-    REQUEST_EDIT_NOTE,
-    REQUEST_DELETE_NOTE,
-    FAIL_SAVE_NOTE,
-    FAIL_DELETE_NOTE
+    FAIL_RECEIVE_NOTES
 } from '../actions';
-
-export function note( state, action ) {
-    switch(action.type) {
-        case SET_NOTE_COLOR:
-            if( state.id !== action.id ) {
-                return state;
-            }
-
-            return Object.assign({}, state, { color: action.color });
-        case ADD_TEXT_NOTE:
-            return {
-                id: action.id,
-                text: action.text,
-                color: action.color
-            };
-        case EDIT_NOTE:
-            if( state.id !== action.id ) {
-                return state;
-            }
-
-            return Object.assign({}, state, action);
-    }
-}
 
 export function notes(state = {
     isFetching: false,
@@ -58,27 +26,6 @@ export function notes(state = {
                 error: action.error,
                 failedAt: action.failedAt
             });
-        case EDIT_NOTE:
-        case SET_NOTE_COLOR:
-            return {
-                isFetching: false,
-                items: state.items.map( n => note(n, action) )
-            };
-        case ADD_TEXT_NOTE:
-            return {
-                isFetching: false,
-                items: [
-                    note( undefined, action),
-                    ... state.items
-                ]
-            };
-        case DELETE_NOTE:
-            return {
-                isFetching: false,
-                items: [
-
-                ]
-            };
         default:
             return state;
     }
